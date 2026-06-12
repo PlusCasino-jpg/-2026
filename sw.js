e4f9bc1e8a3ad49ff0f050f2a3961b1c278cf737
alankabot <mahmoud01moro@gmail.com>
2026-06-12 07:40:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+{
+  "short_name": "كأس العالم في جيبك",
+  "name": "كأس العالم في جيبك – نتائج وجدول مباريات كأس العالم لحظة بلحظة",
+  "icons": [
+    {
+      "src": "/app_icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "/app_icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ],
+  "start_url": "/",
+  "background_color": "#09090b",
+  "theme_color": "#f43f5e",
+  "display": "standalone",
+  "orientation": "portrait"
+}
